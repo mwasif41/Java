20f62db54ffccd5ad43f7a315673562b50b62eb6
Muhammad Wasif <Muhammad Wasif>
2017-08-03 20:11:02 +0500

    singleton updated

--- assignment#1/singleton design pattern/runProg.java
+++ assignment#1/singleton design pattern/runProg.java
@@ -4,11 +4,11 @@ public class runProg {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// client want singleton multiple times and gets the same instance
-		singleton.getSingleton().printSingleton();
-
-		singleton.getSingleton().printSingleton();
-
-		singleton.getSingleton().printSingleton();
-	}
+		for(int i=0; i<5; i++){
+		singleton s = singleton.getSingleton();
+		System.out.println(s);
+		}
+		
+}
 
 }
